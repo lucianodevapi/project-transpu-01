41785abe0c75a781e3e19878d6538585fce53a35
Luciano de Paula <luciano.desenvolvedor@gmail.com>
2024-01-22 10:12:50 -0300

    commit starts from project

---
+++ ms-info/src/test/java/com/marketinginapp/startup/transpu/MsInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marketinginapp.startup.transpu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
